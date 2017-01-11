802dc98c52f24f48128ea8b32fc39fb95a72a8e1
Xavier Laviron <xavier.laviron@gmx.fr>
2017-01-11 20:28:19 +0100

    improve script to convert data files

--- convert_data.R
+++ convert_data.R
@@ -7,39 +7,28 @@ library(stringi)
 
 
 # ------------------------------------------------------------------------------
+# convert and rename all data files
+
+# define patterns to search associated with cat names
+patterns <- c("baghera", "teddy", "jah", "mistigris", "berlioz", "fury", "symba", "kitkat", "zo[eé]")
+cats     <- c("baghera", "teddy", "jah", "mistigris", "berlioz", "fury", "symba", "kitkat", "zoe")
+
 for (src in list.files("data", full.names = TRUE)) {
   # get file extension
   ext <-
     stri_split(src, fixed = ".", simplify = TRUE) %>%
     .[length(.)]
 
-  # get cat name by reading the file name
-  # all_names <- c("baghera", "teddy", "jah", "mistigris", "berlioz", "fury", "symba", "kitkat", "zoe")
-  if (grepl(pattern = "baghera", x = src, ignore.case = TRUE)) {
-    cat_name <- "baghera"
-  } else if (grepl(pattern = "teddy", x = src, ignore.case = TRUE)) {
-    cat_name <- "teddy"
-  } else if (grepl(pattern = "jah", x = src, ignore.case = TRUE)) {
-    cat_name <- "jah"
-  } else if (grepl(pattern = "mistigris", x = src, ignore.case = TRUE)) {
-    cat_name <- "mistigris"
-  } else if (grepl(pattern = "berlioz", x = src, ignore.case = TRUE)) {
-    cat_name <- "berlioz"
-  } else if (grepl(pattern = "fury", x = src, ignore.case = TRUE)) {
-    cat_name <- "fury"
-  } else if (grepl(pattern = "symba", x = src, ignore.case = TRUE)) {
-    cat_name <- "symba"
-  } else if (grepl(pattern = "kitkat", x = src, ignore.case = TRUE)) {
-    cat_name <- "kitkat"
-  } else if (grepl(pattern = "zo[eé]", x = src, ignore.case = TRUE)) {
-    cat_name <- "zoe"
-  } else if (grepl(pattern = "duchesse", x = src, ignore.case = TRUE)) {
-    cat_name <- "duchesse"
-  } else {
-    cat_name <- "unknown"
+  # get cat name
+  for (i in all_names) {
+    if (grepl(pattern = i, x = src, ignore.case = TRUE)) {
+      cat_name <- cats[patterns == i]
+      break
+    }
+    if (i == patterns[length(patterns)]) {
+      print(src)
+      stop("Cat name cannot be found in that file")
   }
-  if (cat_name == "unknown") 
-    stop(cat(src))
 
   if (ext == "gpx") {
     # file is gpx: convert it to csv and rename it
