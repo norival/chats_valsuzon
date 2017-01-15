b59907656ad2cb32752127c0dac0f6a0774799f5
Xavier Laviron <xavier.laviron@gmx.fr>
2017-01-15 22:41:54 +0100

    adds quick check of file content

--- R/convert_data.R
+++ R/convert_data.R
@@ -14,10 +14,29 @@ patterns <- c("baghera", "duchesse", "teddy", "jah", "mistigris", "berlioz", "fu
 cats     <- c("baghera", "duchesse", "teddy", "jah", "mistigris", "berlioz", "fury", "symba", "kitkat", "zoe")
 
 for (src in list.files(data_old_dir, full.names = TRUE)) {
+  # print the file name, it's cool
+  print(src)
+
   # get file extension
   ext <-
     stri_split(src, fixed = ".", simplify = TRUE) %>%
     .[length(.)]
+
+  # quickly check if file type is consistent with its extension
+  a <- readLines(src, n = 1)
+
+  if (grepl(a, pattern = "Date")) {
+    file_type <- "csv"
+  } else {
+    file_type <- "gpx"
+  }
+
+  # if not, print the file name and stop the script
+  if (file_type != ext) {
+    paste("The file", src, "is marked as", ext, "but is not",
+          "Please check that file!") %>%
+      stop()
+  }
 
   # get cat name
   for (i in patterns) {
