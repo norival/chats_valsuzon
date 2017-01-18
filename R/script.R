e2144fff1c112cc858e1dc02f5cb0c681eee7c09
Xavier Laviron <xavier.laviron@gmx.fr>
2017-01-18 09:08:32 +0100

    changes script to fit with new data

--- R/script.R
+++ R/script.R
@@ -21,15 +21,6 @@ library(ggmap)
 # download.file(map[1], z, mode = "wb")
 # pic <- readPNG(source = z)
 
-# get coordinates from cat dataframe
-jah_xy <- cbind(jah$Latitude, jah$Longitude)
-
-# convert the coordinates into spatial object
-jah_xysp <- SpatialPoints(jah_xy)
-
-# compute the habitat range with mcu method
-jah_mc <- mcp(jah_xysp)
-
 # get the map from google maps
 # better looking maps can be obtained from stamen but it does not work all the
 # time, might have some problems
@@ -42,9 +33,35 @@ map <- get_map(location = c(lon = 4.902, lat = 47.410),
                # maptype = "watercolor",
                filename = "maps/suzon")
 
-# plot the map with the data of jah
-# ggplot2 uses fortify() to get the coordinates from the spatial object
+# ------------------------------------------------------------------------------
+# map each territory on one map
+#
+# empty dataframe to store mcp results
+cats_mc <- data.frame()
+
+for (i in levels(as.factor(cats$cat_name))) {
+  # get the data for the cat 'i'
+  the_cat <- cats[cats$cat_name == i, ]
+
+  # get the coordinates into a 'SpatialPoints' objects
+  the_cat_xy <-
+    cbind(the_cat$Latitude, the_cat$Longitude) %>%
+    SpatialPoints()
+
+  # compute the home range estimation
+  the_cat_mc <-
+    mcp(the_cat_xy) %>%
+    fortify()
+  # fill the id field with the name of the cat
+  the_cat_mc$id <- i
+
+  # add the dataframe to cats_mc dataframe
+  cats_mc <- rbind.data.frame(cats_mc, fortify(the_cat_mc))
+
+}
+
+# plot the map
 ggmap(map,
       extent = "device",
-      base_layer = ggplot(data = jah_mc, aes(x = lat, y = long))) +
-  geom_polygon(alpha = 0.7)
+      base_layer = ggplot(data = cats_mc, aes(x = lat, y = long, fill = id))) +
+  geom_polygon(alpha = 0.6)
